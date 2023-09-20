290e257f9867e9292fb19f6a1f7b25576b3de0a3
Matthew Lim <matthewlimcenterdiv@gmail.com>
2023-09-21 04:50:31 +1000

    added stuff

--- src/App.js
+++ src/App.js
@@ -18,17 +18,18 @@ import { TikTok } from "react-tiktok";
 import AudioPlayer from './AudioPlayer';
 
 import image1 from './images/image1.jpg';
-import image2 from './images/image2.jpg';
+import image2 from './images/image6.jpg';
 import image3 from './images/image3.jpg';
 import image4 from './images/image4.jpg';
 import image5 from './images/image5.jpg';
-import image6 from './images/image6.jpg';
+import image6 from './images/image13.png';
 import image7 from './images/image7.jpg';
 import image8 from './images/image8.jpg';
 import image9 from './images/image9.jpg';
 import image10 from './images/image10.jpg';
 import image11 from './images/image11.jpg';
 import image12 from './images/image12.jpg';
+import image13 from './images/image2.jpg';
 
 import {
   Card,
@@ -78,7 +79,7 @@ function App() {
   }); */
   new ScrollCarousel(".my-carousel")
 
-  const images = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12];
+  const images = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13];
 
   return (
     <div className="App">
@@ -323,7 +324,7 @@ function App() {
 
     <div className='bg-[#FFFFFF] pb-20 pr-20 pl-20' >
         <div className='flex flex-col justify-center items-center pb-6'>
-          <img src={kawshomie} className='rounded-3xl h-64'/>
+          <img src={kawshomie} className='rounded-3xl h-64 hover:scale-105'/>
         </div>
 
         <div className='text-4xl text-[#B18FCF] p-4 font-bold'>
@@ -381,7 +382,7 @@ function App() {
 
     <div className='bg-[#FFFFFF] pb-20 pr-20 pl-20' >
         <div className='flex flex-col justify-center items-center pb-6'>
-          <img src={sisyphus} className='rounded-3xl h-64'/>
+          <img src={sisyphus} className='rounded-3xl h-64 hover:scale-105'/>
         </div>
         
 
