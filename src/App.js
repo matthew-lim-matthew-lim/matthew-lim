7254c8c2d4780e9355ed05ffe8f0e0e9d2b5e605
Matthew Lim <matthewlimcenterdiv@gmail.com>
2023-09-20 19:39:05 +1000

    Additional updates

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom'
 import csesocWholesome from './csesocTeam.jpg'
 import me from "./me.glb"
 import pondering from "./pondering.jpg"
+import sorting from "./sorting.png"
 /* import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSquareCaretDown } from '@fortawesome/free-solid-svg-icons'; */
 
@@ -192,6 +193,10 @@ function App() {
     </div>
 
     <div className='bg-slate-100 p-20' >
+    <div className='flex flex-row'>
+
+    </div>
+
         <div className='text-4xl text-[#B18FCF] p-4 font-bold'>
           Vision philosophy/background
         </div>
@@ -201,59 +206,76 @@ function App() {
         <div className='text-2xl mb-6'>
           I'll explain some of my experiences, and the valuable things they have taught me.
         </div>
-        <div  className='text-xl p-2 text-[#978897] font-semibold'>
-          COOPSoc Exec team
-        </div>
-        <div className='flex flex-row'>
-          <div  className='text-xl p-2'>
-            Working in the COOPSoc exec team enabled me to see firsthand the systems 
-            that enable the efficient management of teams, agendas and record-keeping.
+      <div className='flex flex-row'>
+        <div>
+          <div  className='text-xl p-2 mt-4 text-[#978897] font-semibold'>
+            COOPSoc Exec team
           </div>
-          <div  className='text-l p-2'>
-            One impactful experience in this role was when I overstepped and was too authoritative in my leadership style 
-            towards a subcom member. I reflected upon this and realised that I was suppressing their vision.
-            As such, I have grown to value my trust in others, enabling 
-            others to lead and instruct. This improves team morale, and improves team efficiency.
+
+          <div className='flex flex-row bg-[#EFEFFB] rounded-2xl p-3 hover:scale-105'>
+            <div  className='text-xl p-2'>
+              Working in the COOPSoc exec team enabled me to see firsthand the systems 
+              that enable the efficient management of teams, agendas and record-keeping.
+            </div>
+            <div
+            class="inline-block w-0.5 self-stretch bg-slate-700 opacity-50">
+            </div>
+            <div  className='text-l p-2'>
+              One impactful experience in this role was when I overstepped and was too authoritative in my leadership style 
+              towards a subcom member. I reflected upon this and realised that I was suppressing their vision.
+              As such, I have grown to value my trust in others, enabling 
+              others to lead and instruct. This improves team morale, and improves team efficiency.
+            </div>
           </div>
-        </div>
 
 
 
-        <div  className='text-xl p-2 text-[#978897] font-semibold'>
-          CSESoc Dev Content
-        </div>
+          <div  className='text-xl mt-4 p-2 text-[#978897] font-semibold'>
+            CSESoc Dev Content
+          </div>
 
-        <div className='flex flex-row'>
-          <div  className='text-xl p-2'>
-            Creating content for CSESoc allowed me to refine my personal values as a member 
-            of a university society. This is because, in order to create engaging content, 
-            I needed to discern the values and interests of society members.
+          <div className='flex flex-row bg-[#EFEFFB] rounded-2xl p-3 hover:scale-105'>
+            <div  className='text-xl p-2'>
+              Creating content for CSESoc allowed me to refine my personal values as a member 
+              of a university society. This is because, in order to create engaging content, 
+              I needed to discern the values and interests of society members.
+            </div>
+            <div
+              class="inline-block w-0.5 self-stretch bg-slate-700 opacity-50">
+            </div>
+            <div  className='text-l p-2'>
+              The most impactful experience in this role was discussing with my directors
+              Omar and Rachel, 'What makes a good director?'. It was through this 
+              discussion where I began to reshape my perspective on leadership - being that 
+              the leader must put their followers first, and that the leader must 
+              always show empathy towards their followers.
+            </div>
           </div>
-          <div  className='text-l p-2'>
-            The most impactful experience in this role was discussing with my directors
-            Omar and Rachel, 'What makes a good director?'. It was through this 
-            discussion where I began to reshape my perspective on leadership - being that 
-            the leader must put their followers first, and that the leader must 
-            always show empathy towards their followers.
-          </div>
-        </div>
 
-        <div  className='text-xl p-2 text-[#978897] font-semibold'>
-          PISoc IT director
-        </div>
-        <div className='flex flex-row'>
-          <div  className='text-xl p-2'>
-            Managing a team enabled me to experience firsthand the administrative difficulties 
-            faced in leading a team, and to experiment with processes to resolve this.
+          <div  className='text-xl p-2 mt-4 text-[#978897] font-semibold'>
+            PISoc IT director
           </div>
-          <div  className='text-l p-2'>
-            A challenge I faced in this role was communication and team-wide productivity.
-            Here, I was mentored by an exec, Yash, who taught me practical methods to 
-            better manage and utilise the team's resources. Some of these ideas challenged 
-            the systems put in place through technology (eg. relying on when2meet for team meetings), 
-            and therefore increased the scope of leadership techniques that I know.
+          <div className='flex flex-row bg-[#EFEFFB] rounded-2xl p-3 hover:scale-105'>
+            <div  className='text-xl p-2'>
+              Managing a team enabled me to experience firsthand the administrative difficulties 
+              faced in leading a team, and to experiment with processes to resolve this.
+            </div>
+            <div
+              class="inline-block w-0.5 self-stretch bg-slate-700 opacity-50">
+            </div>
+            <div  className='text-l p-2'>
+              A challenge I faced in this role was communication and team-wide productivity.
+              Here, I was mentored by an exec, Yash, who taught me practical methods to 
+              better manage and utilise the team's resources. Some of these ideas challenged 
+              the systems put in place through technology (eg. relying on when2meet for team meetings), 
+              and therefore increased the scope of leadership techniques that I know.
+            </div>
           </div>
         </div>
+        <div className='basis-4/5'>
+          <img src={sorting} className='ml-10 mr-10 mt-10 rounded-2xl' />
+        </div>
+      </div>
 
     </div>
 
