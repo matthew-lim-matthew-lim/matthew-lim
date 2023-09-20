d8edd22c757b8d8fab4a7c73c0ad6edfc30b2c4b
Matthew Lim <matthewlimcenterdiv@gmail.com>
2023-09-20 17:05:30 +1000

    add content

--- src/App.js
+++ src/App.js
@@ -73,11 +73,11 @@ function App() {
       </header>
 
     <div className='bg-white p-20' >
-      <div className='flex flex-row'>
-        <div>
-          <div className='text-4xl text-[#2c2c34] p-6'>
+    <div className='text-4xl text-[#B18FCF] p-4 font-bold'>
             CSESoc has greatly enriched my university experience.
           </div>
+      <div className='flex flex-row'>
+        <div>
           <div className='text-4xl text-[#2c2c34] p-6'>
             Through CSESoc, I have met so many amazing people with incredible stories and talents.
             It is a community I feel like I belong to, one where I've forged friendships that will last a lifetime.
@@ -100,6 +100,8 @@ function App() {
       </div>
     </div>
 
+      <br></br>
+
     <div className='bg-slate-100 p-20' >
       <div className='text-4xl text-[#B18FCF] p-4 font-bold'>
         Vision Statement
@@ -116,39 +118,58 @@ function App() {
       <div className='text-2xl text-[#B18FCF] p-4 font-semibold'>
       Support and Resources
       </div>
-
+      
       <div>
-        
+        I will provide resources and support to help our leaders realize their personal visions. 
+        This may involve allocating budgets, connecting them with relevant networks, 
+        and providing access to tools and technologies.
       </div>
 
       <div className='text-2xl text-[#B18FCF] p-4 font-semibold'>
       Collaboration
       </div>
 
+      <div>
+        I will encourage collaboration among our leadership team. 
+        By fostering an environment of collaboration and open communication, 
+        we will develop a synergy that will positively influence the broader CSE society.
+      </div>
+
       <div className='text-2xl text-[#B18FCF] p-4 font-semibold'>
       Feedback Mechanisms
+      </div>
+
+      <div>
+      Regular feedback sessions will be conducted to assess progress and make necessary adjustments. 
+      This will involve seeking input from co-presidents, directors, and subcom members to continuously improve our approach.
       </div>
 
       <div className='text-2xl text-[#B18FCF] p-4 font-semibold'>
       Measurable Goals
       </div>
 
-      <div className='text-2xl text-[#B18FCF] p-4 font-semibold'>
-      Celebrating Achievements
+      <div>
+      I will establish measurable goals and key performance indicators to track our progress. 
+      These metrics will help me evaluate the impact of our actions and make data-driven decisions.
       </div>
-
-      2. **Support and Resources:** We will provide resources and support to help our leaders realize their personal visions. This may involve allocating budgets, connecting them with relevant networks, and providing access to tools and technologies.
-
-      3. **Collaboration:** We encourage collaboration among our leadership team. By fostering an environment of collaboration and open communication, we aim to ensure that individual visions align with the greater vision of CSESoc.
-
-      4. **Feedback Mechanisms:** Regular feedback sessions will be conducted to assess progress and make necessary adjustments. This will involve seeking input from co-presidents, directors, and subcom members to continuously improve our approach.
-
-      5. **Measurable Goals:** We will establish measurable goals and key performance indicators (KPIs) to track our progress. These metrics will help us evaluate the impact of our actions and make data-driven decisions.
-
-      6. **Celebrating Achievements:** Recognizing and celebrating the achievements of our leaders is essential. We will acknowledge their contributions and successes, fostering a culture of appreciation and motivation.
 
       </div>
     </div>
+
+{/*     <div className='bg-slate-100 p-20' >
+        <div className='text-4xl text-[#B18FCF] p-4 font-bold'>
+          Vision philosophy
+        </div>
+        <div>
+          My perspective of a successful secretary lies in the challenges and experiences 
+          I have faced as both a leader and follower.
+        </div>
+        <div>
+          This year, as a member of Dev Content subcom, the departure of our amazing director Rachel 
+          inspired me to reconsider what exactly made a great leader.
+        </div>
+
+    </div> */}
 
     </div>
   );
